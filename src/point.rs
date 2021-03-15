862a463d25c0cf2f864ad6680e20f451b70ffd89
Cyril Coutelier <cyril@flat.io>
2021-03-15 22:39:43 +0100

    feat: add getters for vector and point

--- src/point.rs
+++ src/point.rs
@@ -6,4 +6,16 @@ impl Point {
     pub fn new(x: f32, y: f32, z: f32) -> Point {
         Point { data: [x, y, z] }
     }
+
+    pub fn x(self: &Point) -> f32 {
+        self.data[0]
+    }
+
+    pub fn y(self: &Point) -> f32 {
+        self.data[1]
+    }
+
+    pub fn z(self: &Point) -> f32 {
+        self.data[2]
+    }
 }
--- src/vector.rs
+++ src/vector.rs
@@ -6,4 +6,14 @@ impl Vector {
     pub fn new(x: f32, y: f32, z: f32) -> Vector {
         Vector { data: [x, y, z] }
     }
+
+    pub fn x(self: &Vector) -> f32 {
+        self.data[0]
+    }
+    pub fn y(self: &Vector) -> f32 {
+        self.data[1]
+    }
+    pub fn z(self: &Vector) -> f32 {
+        self.data[2]
+    }
 }
