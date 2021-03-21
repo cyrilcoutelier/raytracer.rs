fa12c2ae6493a5c83cb3e3890f360e3ccf72f242
Cyril Coutelier <cyril@flat.io>
2021-03-21 12:53:25 +0100

    feat(vector): add norm computation to vector

--- src/vector.rs
+++ src/vector.rs
@@ -1,10 +1,16 @@
+use std::cell::Cell;
+
 pub struct Vector {
     data: [f32; 3],
+    norm: Cell<Option<f32>>,
 }
 
 impl Vector {
     pub fn new(x: f32, y: f32, z: f32) -> Vector {
-        Vector { data: [x, y, z] }
+        Vector {
+            data: [x, y, z],
+            norm: Cell::new(None),
+        }
     }
 
     pub fn x(self: &Vector) -> f32 {
@@ -15,5 +21,26 @@ impl Vector {
     }
     pub fn z(self: &Vector) -> f32 {
         self.data[2]
+    }
+
+    pub fn get_norm(self: &Vector) -> f32 {
+        match self.norm.get() {
+            Some(norm) => norm,
+            None => {
+                let norm = self.calc_norm();
+                self.norm.set(Some(norm));
+                norm
+            }
+        }
+    }
+
+    fn calc_norm(self: &Self) -> f32 {
+        let pow_sum = self
+            .data
+            .iter()
+            .map(|val| val * val)
+            .fold(0.0, |left, right| left + right);
+        let norm = f32::sqrt(pow_sum);
+        norm
     }
 }
