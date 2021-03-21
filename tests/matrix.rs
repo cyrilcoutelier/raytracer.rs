32a45f8dd85598d592a717ea6895f08231279863
Cyril Coutelier <cyril@flat.io>
2021-03-21 12:52:14 +0100

    test(matrix): add test for dot product

--- tests/matrix.rs
+++ tests/matrix.rs
@@ -228,3 +228,100 @@ fn translate() {
     let expected = Point::new(2.0, 3.0, 4.0);
     assert_eq!(actual, expected);
 }
+
+#[test]
+fn dot_basic() {
+    // Given
+    let matrix = Matrix::new_identity();
+    let translate = Matrix::new_translation(&Vector::new(1.0, 2.0, 3.0));
+
+    // When
+    let actual = matrix.dot(&translate);
+
+    // Then
+    let expected = Matrix::new_translation(&Vector::new(1.0, 2.0, 3.0));
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn dot_basic_reverse() {
+    // Given
+    let matrix = Matrix::new_identity();
+    let translate = Matrix::new_translation(&Vector::new(1.0, 2.0, 3.0));
+
+    // When
+    let actual = translate.dot(&matrix);
+
+    // Then
+    let expected = Matrix::new_translation(&Vector::new(1.0, 2.0, 3.0));
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn dot_chained_translate() {
+    // Given
+    let actual = Matrix::new_identity();
+    let translate1 = Matrix::new_translation(&Vector::new(1.0, 2.0, 3.0));
+    let translate2 = Matrix::new_translation(&Vector::new(2.0, 3.0, 4.0));
+
+    // When
+    let actual = actual.dot(&translate1);
+    let actual = actual.dot(&translate2);
+
+    // Then
+    let expected = Matrix::new_translation(&Vector::new(3.0, 5.0, 7.0));
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn dot_chained_translate_rotate_translate() {
+    // Given
+    let initial = Point::new(0.0, 0.0, -2.0);
+    let matrix = Matrix::new_identity();
+    let matrix = matrix.dot(&Matrix::new_translation(&Vector::new(0.0, 0.0, 1.0)));
+    let matrix = matrix.dot(&Matrix::new_rotation_y(PI / 2.0));
+    let matrix = matrix.dot(&Matrix::new_translation(&Vector::new(0.0, 0.0, -1.0)));
+
+    // When
+    let actual = matrix.dot_point(&initial);
+
+    // Then
+    let expected = Point::new(1.0, 0.0, -1.0);
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn dot_chained_3rotate_2translate() {
+    // Given
+    let initial = Point::new(-1.0, 3.0, -2.0);
+    let matrix = Matrix::new_identity();
+    let matrix = matrix.dot(&Matrix::new_translation(&Vector::new(0.0, 0.0, 1.0))); // -1, 3, -1
+    let matrix = matrix.dot(&Matrix::new_rotation_z(PI / 2.0)); // 3, 1, -1
+    let matrix = matrix.dot(&Matrix::new_rotation_y(PI / 2.0)); // 1, 1, 3
+    let matrix = matrix.dot(&Matrix::new_rotation_x(PI / 2.0)); // 1, 3, -1
+    let matrix = matrix.dot(&Matrix::new_translation(&Vector::new(0.0, 0.0, -1.0))); // 1, 3, -2
+
+    // When
+    let actual = matrix.dot_point(&initial);
+
+    // Then
+    let expected = Point::new(1.0, 3.0, -2.0);
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn dot_chained_3rotate() {
+    // Given
+    let initial = Point::new(-1.0, 0.0, -1.0);
+    let matrix = Matrix::new_identity();
+    let matrix = matrix.dot(&Matrix::new_rotation_z(-PI / 2.0)); // 0, -1, -1
+    let matrix = matrix.dot(&Matrix::new_rotation_x(-PI / 2.0)); // 0, 1, -1
+    let matrix = matrix.dot(&Matrix::new_rotation_y(PI / 2.0)); // 1, 1, 0
+
+    // When
+    let actual = matrix.dot_point(&initial);
+
+    // Then
+    let expected = Point::new(1.0, 1.0, 0.0);
+    assert_eq!(actual, expected);
+}
