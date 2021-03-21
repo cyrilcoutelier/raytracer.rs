423cbbf504ff02dad19071137c99aa2267e4f5d5
Cyril Coutelier <cyril@flat.io>
2021-03-21 15:48:51 +0100

    feat: add random sphere generation

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,13 @@ use raytracer::point::Point;
 use raytracer::renderer::render;
 use raytracer::vector::Vector;
 use raytracer::world::World;
+use raytracer::color::Color;
+use rand::Rng;
+
+const NB_SPHERES: usize = 10;
+const X_DISTANCE: f32 = 10.0;
+const MAX_DISTANCE: f32 = 5.0;
+const MAX_RADIUS: f32 = 3.0;
 
 fn main() -> std::io::Result<()> {
     let mut image = Image::new(config::IMG_WIDTH, config::IMG_HEIGHT, None);
@@ -19,15 +26,20 @@ fn main() -> std::io::Result<()> {
         &image,
     );
     let mut world = World::new();
-    world.add(Box::new(Sphere::new(Point::new(10.0, 0.0, 0.0), 1.0)));
-    world.add(Box::new(Sphere::new(Point::new(10.0, 3.0, 0.0), 1.0)));
-    world.add(Box::new(Sphere::new(Point::new(10.0, -3.0, 0.0), 1.0)));
-    world.add(Box::new(Sphere::new(Point::new(10.0, 0.0, 3.0), 1.0)));
-    world.add(Box::new(Sphere::new(Point::new(10.0, 0.0, -3.0), 1.0)));
-    world.add(Box::new(Sphere::new(Point::new(10.0, 3.0, 3.0), 1.0)));
-    world.add(Box::new(Sphere::new(Point::new(10.0, -3.0, 3.0), 1.0)));
-    world.add(Box::new(Sphere::new(Point::new(10.0, 3.0, -3.0), 1.0)));
-    world.add(Box::new(Sphere::new(Point::new(10.0, -3.0, -3.0), 1.0)));
+
+    let mut rng = rand::thread_rng();
+    for _i in 0..NB_SPHERES {
+        let x = rng.gen_range(X_DISTANCE-MAX_DISTANCE..MAX_DISTANCE + X_DISTANCE);
+        let y = rng.gen_range(-MAX_DISTANCE..MAX_DISTANCE);
+        let z = rng.gen_range(-MAX_DISTANCE..MAX_DISTANCE);
+        let radius = rng.gen_range(0.0..MAX_RADIUS);
+        let red = rng.gen_range(0.0..1.0);
+        let green = rng.gen_range(0.0..1.0);
+        let blue = rng.gen_range(0.0..1.0);
+        let color = Color::new(red, green, blue);
+        let sphere = Sphere::new(Point::new(x, y, z), radius, color);
+        world.add(Box::new(sphere));
+    }
 
     render(&camera, &mut image, &world);
 
